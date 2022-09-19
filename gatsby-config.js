4c9922084cf17a99e77ff9058c01ba801a4f6664
Tofik Khedawala <tofikkhedawala407@gmail.com>
2022-09-19 18:44:36 +0530

    url changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `http://localhost/gatsby/`,
   },
   plugins: [
   	{
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: process.env.WPGRAPHQL_URL || `https://your.fallback.url/graphql`,
+        url: process.env.WPGRAPHQL_URL || `http://localhost/gatsby/graphql`,
       },
     },
   ],
